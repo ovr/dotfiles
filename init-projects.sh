0a0b9d02be8d8f675fe382af3953354d09dec31e
Patsura Dmitry <talk@dmtry.me>
2014-11-22 12:11:45 +0900

    Init base @ovr projects

---
+++ init-projects.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Init my opensource projects"
+cd ~
+mkdir projects
+cd projects
+
+mkdir ovr
+cd ovr
+
+git clone git@github.com:ovr/java-homework.git
+git clone git@github.com:ovr/ovr.github.io.git
+git clone git@github.com:ovr/phalcony.git
+git clone git@github.com:ovr/phalcon-module-skeleton.git
+git clone git@github.com:ovr/phalcon-elasticsearch.git
+git clone git@github.com:ovr/cpp-homework.git
+git clone git@github.com:ovr/cacher.git
+git clone git@github.com:ovr/vk-music-sync.git
+git clone git@github.com:ovr/cacher-ext.git
+git clone git@github.com:ovr/hydrator.git
